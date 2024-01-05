48c349c8f165e461f84515352d63c8621246b3a9
Vishesh-Verma-Work <visheshverma.workin@gmail.com>
2024-01-05 06:46:48 -0800

    added component code, logic, function of password generator, ui, and hooks

--- src/components/Pass.jsx
+++ src/components/Pass.jsx
@@ -1,25 +1,81 @@
-import React, { useCallback,useCallback } from 'react'
+import React, { useCallback, useState } from 'react';
 
 function Pass() {
-    const [length, setLength] = useState(8)
-    const [num, setNum] = useState(false)
-    const [char, setChar] = useState(false)
+  const [length, setLength] = useState(8)
+  const [num, setNum] = useState(false)
+  const [char, setChar] = useState(false)
+  const [password, setPassword] = useState("")
 
-    const [password, setPassword] = useCallback(() => {
-      let pass = ""
-      let str = ""
 
-      
+  const passGen = useCallback(() => {
+    let pass = ""
+    let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-      }, [length,num,char,setPassword]);
-    
+    if (num) {
+      str += "012345678"
+    }
+
+    if (num) {
+      str += "!@#$%&){}:/."
+    }
+
+    for (let i = 1; i < length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass = str.charAt(char)
+    }
+    console.log(str.length);
+    console.log("Bhadu");
+    setPassword(pass)
+
+  }, [length, num, char, setPassword]);
+
 
 
 
   return (
     <>
-    <h1 className='bg-black text-cyan-400 py-3 text-center'>hello bhai kaise hoo aap</h1>
-    
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 py-6 text-orange-500 bg-gray-800'>
+        <h1 className='text-white text-center my-2'>Password Generator</h1>
+        <div className='flex shadow-lg rounde-lg overflow-hidden mb-4 rounded-lg'>
+
+          <input type="text"
+            value={password}
+            className='outline-none w-full py-1 px-3'
+            readOnly
+            placeholder='Password'
+          />
+          <button
+            className='outline-none bg-blue-700 text-white px-3 py-.5 shrink-0'
+          >Copy</button>
+
+        </div>
+        <div className='flex text-sm gap-x-2'>
+          <div className='flex items-center gap-x-1'>
+            <input type="range"
+              min={5}
+              max={24}
+              value={length}
+              className='cursor-pointer'
+              onChange={(e) => { setLength(e.target.value) }}
+            />
+            <label>Length : {length}</label>
+
+          </div>
+          <div className='flex items-center gap-x-1'>
+            <input type="check-box"
+              defaultChecked={num}
+              value={length}
+              id='numInp'
+              onChange={() => {
+                setNum((prev) => !prev);
+              }}
+            />
+            <label>Length : {length}</label>
+          </div>
+        </div>
+
+      </div>
+
     </>
   )
 }
